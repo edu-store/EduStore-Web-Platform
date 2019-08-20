b404a0b78d07c4ebf662a7dfdd317b88c0111919
Jehaann Manzanares <developer@jehaann-manzanares.com>
2019-08-20 00:00:22 -0600

    Completed responsive - menu mobile

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+const McButton = document.querySelector("#McButton");
+const btns__menu = document.getElementById("btns__menu");
+const btn__Sign__up = document.getElementById("btn__Sign__up");
+const McBar1 = document.getElementById("McBar1");
+const McBar2 = document.getElementById("McBar2");
+const McBar3 = document.getElementById("McBar3");
+
+McButton.addEventListener("click", function() {
+  btn__Sign__up.classList.remove("btn-white");
+  btn__Sign__up.classList.add("btn-blue");
+  btns__menu.classList.toggle("active");
+  McButton.classList.toggle("active");
+
+  McBar1.classList.toggle("active");
+  McBar2.classList.toggle("active");
+  //   McBar1.style.animationName = "menu-positivo";
+
+  McBar3.classList.toggle("active");
+});
